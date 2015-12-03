92077f023ee2ff324f1b2464835ea7a1603c47a6
wannabesrevenge <schultz100190@gmail.com>
2015-12-03 15:39:30 -0500

    Working API scraper

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+   app = express(),
+   request = require('request');
+
+//app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//});
+app.get('/api/:id', function(req, res) {
+   var url='http://itunes.apple.com/lookup?', id=req.params.id, q, promise;
+   
+   // If all numbers, it's an APP ID. else its a BUNDLE ID
+   if(/^\d+$/.test(id)) q = 'id=' + id;
+   else q = 'bundleId=' + id;
+
+   url += q;
+   console.log('Getting url', url);
+
+   request
+      .get(url, function(err, resp, body){
+         if(err) return res.status(500).json(err);
+         res.json(JSON.parse(body));
+      })
+      .on('response', function(resp) {
+         res.status(resp.statusCode);
+      });
+});
+app.use(express.static('static'));
+
+app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080);
---
+++ static/itunes.js
@@ -0,0 +1,62 @@
+/**
+ * @author: Brian Schultz <wannabesrevenge>
+ * @date: 2015-12-02
+ * 
+ * iTunes API Helper
+ */
+function initApp() {
+   function clear() {
+      rawData.empty();
+      userRating.empty();
+      contentRating.empty();
+      title.empty();
+   }
+
+   function searchDone(data) {
+      console.log(data);
+      rawData.text(JSON.stringify(data));
+
+      if(data.resultCount < 1) return userRating.text('No results');
+      if(data.resultCount > 1) return userRating.text('Mulitple results returned');
+
+      title.text(data.results[0].trackName);
+      userRating.text(data.results[0].averageUserRating);
+      contentRating.text(data.results[0].contentAdvisoryRating);
+   }
+
+   var app = $('#app'),
+      search = $('#search'),
+      searchBtn = $('#search-btn'),
+      userRating = $('#ur'),
+      contentRating = $('#cr'),
+      rawData = $('#data'),
+      title = $('#title');
+
+   searchBtn.click(function(e) {
+      clear();
+
+      itunes.query(search.val())
+         .done(searchDone)
+         .fail(function(data) {
+            console.log('F', data);
+         });
+   });
+}
+
+function askApple(id) {
+   itunes.query(id).done(function(data){
+      
+   }).fail(function(data) {
+
+   });
+}
+
+$(document).ready(initApp);
+
+var itunes = {
+   query: function(id) {
+      var url='/api/' + id;
+      return  $.get(url);
+   }
+
+};
